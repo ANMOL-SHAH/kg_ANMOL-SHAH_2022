b28ac53801444c3cb88cca924b9c68ab3b16ff35
ANMOL-SHAH <shahanmol655@gmail.com>
2021-04-29 13:33:19 -0400

    Code converting phonetic equivalent of Integers and throwing exception when number is negative integer

---
+++ main.js
@@ -0,0 +1,42 @@
+//Input: Array of Integers
+//Expected output: Array of Strings
+
+// Words 
+var words = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'];
+
+//Stores argument in the nums
+var nums = process.argv.slice(2);
+
+//Stores final output
+var output = [];
+
+
+//function to get sub output for each element in array
+function getSubOutput(num) {
+    //Checking for exception case where if input array consist of number less than 0, then code will remove it from array
+    if (num < 0) {
+        //throws error when negative integer found
+        throw new Error("Negative Integer found in the array. Please Try Again");
+    }
+    else {
+        word = ""
+        for (let j = 0; j < num.length; j++) {
+            word = word + words[num[j]];
+        }
+        output.push(word)
+    }
+}
+
+//checks for length of input array if array is not empty then checks for exceptions
+if (nums.length > 0) {
+    //loops through elements of input array
+    nums.forEach(num => {
+        getSubOutput(num);
+    });
+    //Prints output in stdout
+    // Output variable stores array of strings
+    console.log(output);
+} else {
+    //if array is empty 
+    process.stdout.write("Size of Input is 0");
+}
